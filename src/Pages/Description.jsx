f368d0870c44a23af6361fc80beeeb2b10af3be2
Vader <thesecondshot7@gmail.com>
2025-12-12 06:10:16 -0800

    changes made

--- src/Pages/Description.jsx
+++ src/Pages/Description.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import { Plus, Minus } from "lucide-react";
+
 
 function Description({ descriptionItem, cartItems, setCartItems }) {
   const { id } = useParams();
